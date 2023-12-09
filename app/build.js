d2a3293a95dda5258b072fedd31bf0d24c6476f5
Klaas Van Parys <klaas@ivox.be>
2023-12-09 17:16:04 +0100

    Added a Vue SFC compiler

--- app/build.js
+++ app/build.js
@@ -2,49 +2,65 @@ import fs from "fs";
 
 import copyFiles from "copyFiles";
 
+import { compileSFCs } from 'plain-vue-sfc-compiler';
+
 console.log("Build - cleaning dist");
 
 fs.rmSync("./dist", {recursive: true, force: true});
 
 console.log("Build - copying source app files");
 
-copyFiles([
-	"./src/user/**/*",
-	"./dist/user"
-], {up: 2, all: true}, () => {});
+await new Promise(resolve => {
+	copyFiles([
+		"./src/user/**/*",
+		"./dist/user"
+	], {up: 2, all: true, exclude: ['./**/*.vue']}, resolve);
+});
+
+await compileSFCs("./src/user/", "./dist/user/", {globalCssFile: "./dist/user/css/components.css"});
 
 console.log("Build - copying javascript libraries");
 
-copyFiles([
-	"./node_modules/vue/dist/vue.esm-browser.js",
-	"./node_modules/vue-router/dist/vue-router.esm-browser.js",
-	"./dist/user/js/libs"
-], {up: true}, () => {})
+await new Promise(resolve => {
+	copyFiles([
+		"./node_modules/vue/dist/vue.esm-browser.js",
+		"./node_modules/vue-router/dist/vue-router.esm-browser.js",
+		"./dist/user/js/libs"
+	], {up: true}, resolve);
+});
 
-copyFiles([
-	"./node_modules/@vue/devtools-api/lib/esm/**/*",
-	"./dist/user/js/libs/@vue/devtools-api"
-], {up: 5}, () => {});
+await new Promise(resolve => {
+	copyFiles([
+		"./node_modules/@vue/devtools-api/lib/esm/**/*",
+		"./dist/user/js/libs/@vue/devtools-api"
+	], {up: 5}, resolve);
+});
 
 console.log("Build - copying MultiversX files");
 
-copyFiles([
-	"./node_modules/@multiversx/sdk-core/out/**/*",
-	"./dist/user/js/libs/@multiversx/sdk-core"
-], {up: 4}, () => {});
+await new Promise(resolve => {
+	copyFiles([
+		"./node_modules/@multiversx/sdk-core/out/**/*",
+		"./dist/user/js/libs/@multiversx/sdk-core"
+	], {up: 4}, resolve);
+});
 
 console.log("Build - copying css libraries");
 
-copyFiles([
-	"./node_modules/@fortawesome/fontawesome-free/css/all.min.css",
-	"./dist/user/css/libs"
-], {up: true}, () => {})
+await new Promise(resolve => {
+	copyFiles([
+		"./node_modules/@fortawesome/fontawesome-free/css/all.min.css",
+		"./dist/user/css/libs"
+	], {up: true}, resolve);
+});
 
 console.log("Build - copying fonts");
 
-copyFiles([
-	"./node_modules/@fortawesome/fontawesome-free/webfonts/*",
-	"./dist/user/css/webfonts"
-], {up: true}, () => {})
+await new Promise(resolve => {
+	copyFiles([
+		"./node_modules/@fortawesome/fontawesome-free/webfonts/*",
+		"./dist/user/css/webfonts"
+	], {up: true}, resolve);
+});
 
 console.log("Build - finished");
